5c346bc7ade7c35357cae6cd6e8e213d84617aeb
aeryz <abdullaheryz@protonmail.com>
2020-11-28 01:18:33 +0300

    Starting to builder 08

--- main.rs
+++ main.rs
@@ -6,28 +6,19 @@
 // To run the code:
 //     $ cargo run
 
+
 use derive_builder::Builder;
 
 #[derive(Builder)]
 pub struct Command {
     executable: String,
-    #[builder(each = "arg")]
+    #[builder(eac = "arg")]
     args: Vec<String>,
-    #[builder(each = "env")]
     env: Vec<String>,
     current_dir: Option<String>,
 }
 
-fn main() {
-    let command = Command::builder()
-        .executable("cargo".to_owned())
-        .arg("build".to_owned())
-        .arg("--release".to_owned())
-        .build()
-        .unwrap();
+fn main() {}
 
-    assert_eq!(command.executable, "cargo");
-    assert_eq!(command.args, vec!["build", "--release"]);
-}
 
 
